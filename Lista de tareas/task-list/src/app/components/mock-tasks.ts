2a0855e01421e313685cee959d798abc2e842172
MorganaLA <moriluz97@gmail.com>
2022-10-10 14:24:08 -0300

    mock de tareas

---
+++ Lista de tareas/task-list/src/app/components/mock-tasks.ts
@@ -0,0 +1,26 @@
+import { Task } from "../Task"
+
+export const TASKS:Task[]=[
+    { id: 1,
+      text: "Terminar primer modulo de Angular",
+      day: '5 de agosto a las 12:00',
+      reminder: true  
+    },
+    {id: 2,
+    text: "Hacer las compras para la cena",
+    day: '5 de agosto a las 17:00',
+    reminder: true  
+    },
+    { id: 3,
+        text: "Investigar sobre Boostrap",
+        day: '5 de agosto a las 19:00',
+        reminder: false  
+    },
+    { id: 4,
+        text: "Leer mi libro favorito",
+        day: '5 de agosto a las 21:00',
+        reminder: false  
+    }
+
+
+]
